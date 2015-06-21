d9923fbf67318c1e8fe89519559ed74d2ace1714
ted-burns <teddy.burns.96@gmail.com>
2015-06-21 12:17:00 -0400

    Added .Rhistory and wrote the functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creates a special matrix capable of caching its own inverse
 
-## Write a short comment describing this function
+## Makes the special cache matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set = set, get = get, setinverse =  setinv, 
+         getinverse = getinv)
 }
 
 
-## Write a short comment describing this function
+##Returns the inverse of the matrix and, if the cached matrix
+##is null, caches a new inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv))
+    {
+        message("Getting previously calculated inverse")
+        return(inv)
+    }
+    matrix <- x$get()
+    inv <- solve(matrix)
+    x$setinverse(inv)
+    inv
 }
